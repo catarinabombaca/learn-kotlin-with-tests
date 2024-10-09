d2da5a2b81b2bbb94d68cf8c2f7e15781d2597e4
catarinabombaca <catarina.bombaca@sedex.com>
2024-10-09 17:29:56 +0100

    feat: classes

--- src/main/kotlin/Person.kt
+++ src/main/kotlin/Person.kt
@@ -1,5 +1,19 @@
-class Person(name: String, surname: String) {
+class Person(val name: String, val surname: String) {
+    var children = emptyList<Person>()
+
     init {
         println("Hi, my name is $name $surname.")
+    }
+
+    constructor(name: String, surname: String, parent: Person) : this(name, surname) {
+        parent.addChild(this)
+    }
+
+    fun fullName(): String {
+        return "$name $surname"
+    }
+
+    fun addChild(child: Person) {
+        children += child
     }
 }
--- src/test/kotlin/PersonTest.kt
+++ src/test/kotlin/PersonTest.kt
@@ -4,7 +4,7 @@ import kotlin.test.assertEquals
 
 class PersonTest {
     @Test
-    fun `given a person I get the information of their name and surname`() {
+    fun `when creating a person, then information of their name and surname is printed out`() {
         // How to test the console output: https://blog.mestwin.net/test-the-console-output-println-in-kotlin/
         val output = tapSystemOut {
             Person("Catarina", "Bombaca")
@@ -14,5 +14,36 @@ class PersonTest {
             "Hi, my name is Catarina Bombaca.",
             output.trim()
         )
+    }
+
+    @Test
+    fun `when requesting the full name, then we get the first and last name`() {
+        val name = "Catarina"
+        val surname = "Bombaca"
+
+        val fullName = Person(name, surname).fullName()
+
+        assertEquals("Catarina Bombaca", fullName)
+    }
+
+    @Test
+    fun `when requesting to add children, then a person has a child associated`() {
+        val parent = Person("Catarina", "Bombaca")
+        val child = Person("Wenqing", "Bombaca")
+
+        parent.addChild(child)
+
+        assertEquals(1, parent.children.size)
+        assertEquals(child.fullName(), parent.children[0].fullName())
+    }
+
+    @Test
+    fun `when creating a child, then the child is automatically associated to their parent`() {
+        val parent = Person("Catarina", "Bombaca")
+
+        val child = Person("Wenqing", "Bombaca", parent)
+
+        assertEquals(1, parent.children.size)
+        assertEquals(child.fullName(), parent.children[0].fullName())
     }
 }
